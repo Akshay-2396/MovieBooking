07f4335fbab512120f00cb63b512e14c46a96df2
Akshay-2396 <itsakshay.ak96@gmail.com>
2025-12-04 17:57:54 +0530

    updated

--- backend/controllers/moviesController.js
+++ backend/controllers/moviesController.js
@@ -4,7 +4,7 @@ import Movie from "../models/movieModel.js";
 import path from "path";
 import fs from "fs";
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://moviebooking-yqod.onrender.com";
 
 /* ---------------------- small helpers ---------------------- */
 const getUploadUrl = (val) => {
